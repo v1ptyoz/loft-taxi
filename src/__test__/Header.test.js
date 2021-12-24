273039d628ddbd2ed6aa3a296501a0e35e0575fd
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2021-12-24 18:17:33 +1000

    perf(header.test.js): remove unnecessary imports

--- src/__test__/Header.test.js
+++ src/__test__/Header.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Header } from "../components/Header/Header";
-import { queryByTestId, queryByText, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
  
 describe("Login form tests", () => {
