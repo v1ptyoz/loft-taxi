cf4838c44f21f525ec733a9f5ede867b8b7f192a
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2021-12-29 11:09:20 +1000

    refactor(login.js): delete context, delete formhandler, add new prop

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,36 +2,24 @@ import "./Login.css";
 import logo from "./logo.png"
 import { LoginForm } from "../../components/LoginForm/LoginForm";
 import { RegisterForm } from "../../components/RegisterForm/RegisterForm";
-import { useContext, useState } from "react";
-import dataContext from "../../context";
 import propTypes from "prop-types";
 
 export function Login(props) {
-  let [formType, setFormType] = useState('login'); 
-
-  const context = useContext(dataContext);
-
-  const formHandler = (event) => {
-    event.preventDefault();
-    context.login();
-    props.showPage("main");
-  }
-
   return (
     <div className="login-page">
       <div className="left">
         <img src={logo} alt="Логотип"/>
       </div>
       <div className="right">
-        {formType === 'login' && 
-          <LoginForm linkHandler={() => setFormType("register")} formHandler={formHandler} />}
-        {formType === 'register' && 
-          <RegisterForm linkHandler={() => setFormType("login")} formHandler={formHandler} />}  
+        {props.formType === 'login' && 
+          <LoginForm />}
+        {props.formType === 'register' && 
+          <RegisterForm />}  
       </div>
     </div>
   )
 }
 
 Login.propTypes = {
-  showPage: propTypes.func.isRequired,
+  formType: propTypes.string.isRequired
 }
