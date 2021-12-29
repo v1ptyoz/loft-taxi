901ddfe65d463043512aef75960d96421bfca955
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2021-12-29 11:14:14 +1000

    refactor(map.js): delete prop&proptypes lib, add react router logic(Outlet)

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,10 +1,9 @@
-import { ProfileForm } from "../ProfileForm/ProfileForm";
 import "./Map.css";
-import propTypes from "prop-types";
 import { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
+import { Outlet } from "react-router-dom";
 
-export function Map(props) {
+export function Map() {
   mapboxgl.accessToken = 'pk.eyJ1IjoidjFwdHlveiIsImEiOiJja3hoMXVyYnUyM3dnMnZxcXQyaGhwbXB2In0.MlLKjuFJNF9iMzHLLeT1tg';
   const mapContainer = useRef(null);
 
@@ -20,12 +19,7 @@ export function Map(props) {
   
   return (
     <div className="map" ref={mapContainer}>
-      {props.currentPage === "profile" && <ProfileForm showPage={props.showPage} />}
+      <Outlet />
     </div>
   )
 }
-
-Map.propTypes = {
-  currentPage: propTypes.string.isRequired,
-  showPage: propTypes.func.isRequired
-};
