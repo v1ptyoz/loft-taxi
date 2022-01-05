bf9d60926b9a5fe5c251a54dec35f982498b2362
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-01-05 12:36:01 +1000

    feat(card.js): delete mock data, start using props

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,9 +3,9 @@ import "./Card.css";
 export function Card(props) {
   return (
     <div className="card">
-      <div className="card__field card__field--date">05/08</div>
-      <div className="card__field card__field--number">1234 5678 9012 3456 44</div>
-      <div className="card__field card__field--cvc">123</div>
+      <div className="card__field card__field--date">{props.expiryDate}</div>
+      <div className="card__field card__field--number">{props.cardNumber}</div>
+      <div className="card__field card__field--cvc">{props.cvc}</div>
     </div>
   )
 }
