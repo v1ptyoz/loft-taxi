9f74e844c3fb2a4cbc87097d6d25106d0268ce45
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-01-05 12:00:53 +1000

    feat: add actions, middlewares and reducers for card store

---
+++ src/modules/card/actions.js
@@ -0,0 +1,8 @@
+import { createAction } from "redux-actions";
+
+export const sendCard = createAction("loft-taxi/card/sendCard");
+export const setCard = createAction("loft-taxi/card/setCard");
+export const getCard = createAction("loft-taxi/card/setCard");
+export const loadCard = createAction("loft-taxi/card/loadCard");
+
+
