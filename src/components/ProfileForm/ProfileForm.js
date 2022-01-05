fc640367a19d89f28a995804f5edd7a100f17e45
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-01-05 12:38:32 +1000

    refactor(profileform.js): using redux and local state, click on button call sendCard()

--- src/components/ProfileForm/ProfileForm.js
+++ src/components/ProfileForm/ProfileForm.js
@@ -2,12 +2,21 @@ import { Button } from "../Button/Button";
 import { Card } from "../Card/Card";
 import "./ProfileForm.css";
 import { useNavigate } from "react-router-dom";
+import { connect } from "react-redux";
+import { sendCard, getCard } from "../../modules/card"
+import { useState } from "react";
 
-export function ProfileForm() {
+function ProfileForm(props) {
+  const [cardNumber, setCardNumber] = useState(props.card.cardNumber);
+  const [expiryDate, setExpiryDate] = useState(props.card.expiryDate);
+  const [cardName, setCardName] = useState(props.card.cardName);
+  const [cvc, setCvc] = useState(props.card.cvc);
+
   const navigate = useNavigate()
 
-  const submit = (event) => {
+  const submit = async (event) => {
     event.preventDefault();
+    await props.sendCard({cardNumber, expiryDate, cardName, cvc, token: props.token})
     navigate("/");
   }
 
@@ -23,25 +32,25 @@ export function ProfileForm() {
             <div className="form__left">
               <label className="form__input-text">
                 <span>Имя владельца</span>
-                <input type="text" />
+                <input type="text" value={cardName} onInput={(event) => { setCardName(event.target.value) }} />
               </label>
               <label className="form__input-text">
                 <span>Номер карты</span>
-                <input type="number" />
+                <input type="number" value={cardNumber} onInput={(event) => { setCardNumber(event.target.value) }} />
               </label>
               <div className="form__group">
                 <label className="form__input-text">
                   <span>MM/YY</span>
-                  <input type="text" />
+                  <input type="text" value={expiryDate} onInput={(event) => { setExpiryDate(event.target.value) }} />
                 </label>
                 <label className="form__input-text">
                   <span>CVC</span>
-                  <input type="number" />
+                  <input type="number" value={cvc} onInput={(event) => { setCvc(event.target.value) }}/>
                 </label>
               </div>
             </div>
             <div className="form__right">
-              <Card />
+              <Card cardName={cardName} cardNumber={cardNumber} expiryDate={expiryDate} cvc={cvc} />
             </div>
           </div>
           <div className="form__buttons">
@@ -52,3 +61,8 @@ export function ProfileForm() {
     </div>
   )
 }
+
+export default connect(
+  (state) => ({card: state.card, token: state.user.token}),
+  { sendCard, getCard }
+)(ProfileForm);
