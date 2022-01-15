617c87f38eb9d990545dec90be3aa0565b92f18b
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-01-15 12:26:30 +1000

    feat: add RoutesForm component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,6 +2,7 @@ import "./Map.css";
 import { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
 import { Outlet } from "react-router-dom";
+import RoutesForm from "../RoutesForm/RoutesForm";
 
 export function Map() {
   mapboxgl.accessToken = 'pk.eyJ1IjoidjFwdHlveiIsImEiOiJja3hoMXVyYnUyM3dnMnZxcXQyaGhwbXB2In0.MlLKjuFJNF9iMzHLLeT1tg';
@@ -18,8 +19,11 @@ export function Map() {
   }, [])
   
   return (
-    <div className="map" ref={mapContainer} data-testid="map">
-      <Outlet />
+    <div className="map">
+      <div className="map__container" ref={mapContainer} data-testid="map">
+        <RoutesForm />
+        <Outlet />
+      </div>
     </div>
   )
 }
---
+++ src/components/RoutesForm/RoutesForm.js
@@ -0,0 +1,90 @@
+import "./RoutesForm.css";
+import Box from "@mui/material/Box"
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import { useEffect, useState } from "react";
+import { Button } from "../Button/Button";
+import { connect } from "react-redux";
+import { getAddresses } from "../../modules/addresses";
+
+function RoutesForm(props) {
+  const [from, setFrom] = useState('');
+  const [to, setTo] = useState('');
+
+  useEffect(() => {
+    if (props.list.length === 0) {
+      props.getAddresses();
+    }
+  }, [props.list.length])
+
+  const handleFrom = (event) => {
+    setFrom(event.target.value);
+  };
+
+  const handleTo = (event) => {
+    setTo(event.target.value);
+  };
+
+  const submit = (event) => {
+    event.preventDefault();
+  }
+
+  return (
+      <div className="routes">
+          <form onSubmit={submit}>
+            <Box>
+              <FormControl variant="standard" sx={{ m: 1, minWidth: 400 }}>
+                <InputLabel id="from-label">Откуда</InputLabel>
+                <Select
+                  labelId="from-label"
+                  id="from-select-standard"
+                  value={from}
+                  onChange={handleFrom}
+                  label="Откуда"
+                >
+                  <MenuItem value="">
+                    <em>Откуда</em>
+                  </MenuItem>
+
+                  {props.list.map(element => {
+                    if (element !== to) {
+                      return <MenuItem key={element} value={element}>{element}</MenuItem>
+                    } else return null;
+                  })}
+                  
+                </Select>
+              </FormControl>
+            </Box>
+            <Box>
+              <FormControl variant="standard" sx={{ m: 1, minWidth: 400, mb: 5 }}>
+                <InputLabel id="to-label">Куда</InputLabel>
+                <Select
+                  labelId="to-label"
+                  id="to-select-standard"
+                  value={to}
+                  onChange={handleTo}
+                  label="Куда"
+                >
+                  <MenuItem value="">
+                    <em>Куда</em>
+                  </MenuItem>
+                  {props.list.map(element => {
+                    if (element !== from) {
+                      return <MenuItem key={element} value={element}>{element}</MenuItem>
+                    } else return null;
+                  })}
+                </Select>
+              </FormControl>
+            </Box>
+            <Button caption="Заказать" type="submit" />
+          </form>
+      </div>
+  )
+}
+
+export default connect(
+  (state) => ({list: state.addresses.list}),
+  { getAddresses }
+)(RoutesForm);
