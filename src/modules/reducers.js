c4ba55e43e7126c9c8a59a2521d92b5217cd6461
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-01-15 12:30:17 +1000

    feat: connect addresses reducer to state

--- src/modules/reducers.js
+++ src/modules/reducers.js
@@ -1,10 +1,12 @@
 import card from './card';
-import user from './user'
+import user from './user';
+import addresses from './addresses';
 import { combineReducers } from "redux";
 
 const appState = combineReducers({
   user,
-  card
+  card,
+  addresses
 });
 
 export default appState;
