40c058034cca6862705e7164a9672673a1626afa
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-01-16 14:28:51 +1000

    refactor: use external variable for map and sending it to routesForm as prop

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -7,21 +7,23 @@ import RoutesForm from "../RoutesForm/RoutesForm";
 export function Map() {
   mapboxgl.accessToken = 'pk.eyJ1IjoidjFwdHlveiIsImEiOiJja3hoMXVyYnUyM3dnMnZxcXQyaGhwbXB2In0.MlLKjuFJNF9iMzHLLeT1tg';
   const mapContainer = useRef(null);
-
-  useEffect(() => {
-    const map = new mapboxgl.Map({
+  const map = useRef(null);
+  
+  useEffect( () => {
+    map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/light-v10',
-      center: [37.617587104605946,55.7513142251542],
-      zoom: 15
+      center: [30.3228,59.9327],
+      zoom: 12
     });
+    
     return () => map.remove();
   }, [])
   
   return (
     <div className="map">
       <div className="map__container" ref={mapContainer} data-testid="map">
-        <RoutesForm />
+        <RoutesForm map={map} />
         <Outlet />
       </div>
     </div>
