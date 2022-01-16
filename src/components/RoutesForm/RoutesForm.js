c123546edd6615d3f135b098c28407e395752dca
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-01-16 18:58:47 +1000

    refactor: draw route now is in the routes form component

--- src/components/RoutesForm/RoutesForm.js
+++ src/components/RoutesForm/RoutesForm.js
@@ -13,10 +13,49 @@ import { getRoute } from "../../modules/route";
 function RoutesForm(props) {
   const [from, setFrom] = useState('');
   const [to, setTo] = useState('');
+  function drawRoute(map, coordinates) {
+    if (map.getLayer("route")) {
+      map.removeLayer("route");
+    }
+    if (map.getSource("route")) {
+      map.removeSource("route");
+    }
+    map.flyTo({
+      center: coordinates[0],
+      zoom: 15
+    });
+   
+    map.addLayer({
+      id: "route",
+      type: "line",
+      source: {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates
+          }
+        }
+      },
+      layout: {
+        "line-join": "round",
+        "line-cap": "round"
+      },
+      paint: {
+        "line-color": "#ffc617",
+        "line-width": 8
+      }
+    });
+  };
 
   useEffect(() => {
     if (props.list.length === 0) {
       props.getAddresses();
+    }
+    if (props.points.length > 0) {
+      drawRoute(props.map.current, props.points);
     }
   }, [props])
 
