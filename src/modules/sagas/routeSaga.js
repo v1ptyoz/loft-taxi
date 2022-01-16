87554bc0cd500a8e075ed91440d06268df8ed531
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-01-16 18:57:09 +1000

    refactor: now saga using setRoute action

--- src/modules/sagas/routeSaga.js
+++ src/modules/sagas/routeSaga.js
@@ -1,50 +1,15 @@
 import { doGetRoute } from "../api";
-import { getRoute } from "../route/actions";
-import { takeEvery, call } from "redux-saga/effects";
+import { getRoute, setRoute } from "../route/actions";
+import { takeEvery, call, put } from "redux-saga/effects";
 
 export function* routeSaga() {
   yield takeEvery(getRoute, function*({payload}) {
-    const [address1, address2, map] = payload;
+    const [address1, address2] = payload;
     const response = yield call(doGetRoute, [address1, address2]);
     if (response.status === 200) {
-      drawRoute(map, response.data)
+      yield put(setRoute(response.data));
     }
   })
 }
 
-function drawRoute(map, coordinates) {
-  if (map.getLayer("route")) {
-    map.removeLayer("route");
-  }
-  if (map.getSource("route")) {
-    map.removeSource("route");
-  }
-  map.flyTo({
-    center: coordinates[0],
-    zoom: 15
-  });
- 
-  map.addLayer({
-    id: "route",
-    type: "line",
-    source: {
-      type: "geojson",
-      data: {
-        type: "Feature",
-        properties: {},
-        geometry: {
-          type: "LineString",
-          coordinates
-        }
-      }
-    },
-    layout: {
-      "line-join": "round",
-      "line-cap": "round"
-    },
-    paint: {
-      "line-color": "#ffc617",
-      "line-width": 8
-    }
-  });
-};
+
