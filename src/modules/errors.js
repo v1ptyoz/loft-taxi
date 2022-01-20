352367464cf033d0df1c8b2a109f53e2c3ec448d
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-01-20 13:30:33 +1000

    feat: new object for error messages

---
+++ src/modules/errors.js
@@ -0,0 +1,7 @@
+const messages = {
+  required: "Поле обязательно для заполнения",
+  wrong: "Неверно заполнено поле",
+  minLen: (value) => `Минимальная длина - ${value} символов`
+}
+
+export default messages;
