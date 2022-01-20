47160408791459d148a3a205da8bbdf1bb9276c5
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-01-20 14:42:07 +1000

    feat: add maxLen func and prefixier for words

--- src/modules/errors.js
+++ src/modules/errors.js
@@ -1,7 +1,22 @@
 const messages = {
   required: "Поле обязательно для заполнения",
   wrong: "Неверно заполнено поле",
-  minLen: (value) => `Минимальная длина - ${value} символов`
+  minLen: (value) => `Минимальная длина - ${value} ${prefixier(value)}`,
+  maxLen: (value) => `Максимальная длина - ${value} ${prefixier(value)}`
+}
+
+const prefixier = (value) => {
+  const n = value % 10;
+  if (n > 10 && n < 20) {
+      return 'символов';
+  }
+  if (n > 1 && n < 5) {
+      return 'символа';
+  }
+  if (n === 1) {
+      return 'символ';
+  }
+  return 'символов';
 }
 
 export default messages;
