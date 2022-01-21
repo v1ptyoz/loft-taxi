2d55f84a1155f1f2e3517feec56480a726334b8f
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-01-21 15:06:52 +1000

    fix: prefixier now is working between 10 and 20

--- src/modules/errors.js
+++ src/modules/errors.js
@@ -1,13 +1,14 @@
 const messages = {
   required: "Поле обязательно для заполнения",
   wrong: "Неверно заполнено поле",
+  nows: "Поле должно содержать один пробел между словами",
   minLen: (value) => `Минимальная длина - ${value} ${prefixier(value)}`,
   maxLen: (value) => `Максимальная длина - ${value} ${prefixier(value)}`
 }
 
 const prefixier = (value) => {
   const n = value % 10;
-  if (n > 10 && n < 20) {
+  if (value > 10 && value < 20) {
       return 'символов';
   }
   if (n > 1 && n < 5) {
