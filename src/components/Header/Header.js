dcd9e4faa9ef4732326ef5b9beb43e98f8536bbc
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-01-25 07:00:16 +1000

    feat: styling for 320px screens

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,47 +1,48 @@
-import "./Header.css"
 import {Logo} from 'loft-taxi-mui-theme';
 import {AppBar, Box, Toolbar} from '@mui/material';
 import { NavLink } from 'react-router-dom';
 import { connect } from "react-redux";
 import { logOut } from "../../modules/user";
+import "./Header.css"
 
 function Header(props) {
+  
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar sx={{ backgroundColor: '#1C1A19', position: 'static'}}>
-        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between'}}>
-          <Box>
-            <Logo 
-              sx={{ mr: 2 }}
-            />
-          </Box>
-          <Box sx={{display: "flex", gap: "44px"}}>
-            <NavLink 
-              to="/" 
-              end 
-              className={({ isActive }) => isActive ? "header__link header__link--active" : "header__link" }
-              >
-              Карта
-            </NavLink>
-            <NavLink 
-              to="profile" 
-              end 
-              className={({ isActive }) => isActive ? "header__link header__link--active" : "header__link" }
-              >
-              Профиль
-            </NavLink>
-            <NavLink 
-              to="/login" 
-              end 
-              className={({ isActive }) => isActive ? "header__link header__link--active" : "header__link" }
-              onClick={props.logOut}
-              >
-              Выход
-            </NavLink>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar sx={{ backgroundColor: '#1C1A19', position: 'static'}}>
+          <Toolbar sx={{ display: 'flex' }} className='header__topline'>
+            <Box>
+              <Logo 
+                sx={{ mr: 2 }}
+              />
+            </Box>
+            <Box sx={{display: "flex", gap: "44px"}}>
+              <NavLink 
+                to="/" 
+                end 
+                className={({ isActive }) => isActive ? "header__link header__link--active" : "header__link" }
+                >
+                Карта
+              </NavLink>
+              <NavLink 
+                to="profile" 
+                end 
+                className={({ isActive }) => isActive ? "header__link header__link--active" : "header__link" }
+                >
+                Профиль
+              </NavLink>
+              <NavLink 
+                to="/login" 
+                end 
+                className={({ isActive }) => isActive ? "header__link header__link--active" : "header__link" }
+                onClick={props.logOut}
+                >
+                Выход
+              </NavLink>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
   )
 }
 
